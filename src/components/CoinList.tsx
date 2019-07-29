a15b1a12e4c40cbcfd38b435b6cf53dd500efc4d
Petr Klimenko <petrklim@yandex.ru>
2019-07-29 11:05:35 +0300

    add nice styling for components

--- src/components/CoinList.tsx
+++ src/components/CoinList.tsx
@@ -6,22 +6,29 @@ import { CoinListStoreContext } from "../state/CoinList";
 import CoinRow from "./CoinRow";
 import ErrorPopup from "./ErrorPopup";
 
-// TODO: Apply nice colorscheme
-// TODO: Based on sign of change use different color: green on positive and red on Negative
-// TODO: Show coins Icons
-// TODO: Show capitalization as a progressbar
-
 // TODO: Strict Typescript
 
 // TODO: Add tests with react-native-testing-library
 
+// TODO: Show coins Icons
 // TODO: Switch base currency (default: USD)
-// TODO: Refresh Button in the header
+// TODO: Refresh Button in the header?
 // TODO: Click on coin: open coin profile
-// TODO: Run on iOS
 
-// TODO: refactor make request: return Result
+// TODO: Run on iOS: there is a problem with ios simulator
 
+// TODO: refactor make request: return Result, stop throwing errors
+
+/**
+ * The main component
+ *
+ * Based on a standard FlatList component.
+ * It implements refresh and scroll loading features.
+ * Came across with the problem when tried to pass mobx stor actions
+ * directly to FlatList component props. In order to make it work
+ * used inline arrow function, which is not good practice.
+ * It's
+ */
 const CoinList = observer(() => {
   const coinListStore = useContext(CoinListStoreContext);
 
@@ -33,20 +40,14 @@ const CoinList = observer(() => {
     <View style={styles.container}>
       <Text style={styles.header}>Coins</Text>
       <FlatList
+        style={styles.list}
         data={coinListStore.list}
         keyExtractor={item => `${item.id}`}
-        renderItem={({ item, index }) => (
-          <CoinRow coin={item} index={index} height={ITEM_HEIGHT} />
-        )}
+        renderItem={({ item }) => <CoinRow coin={item} />}
         onRefresh={() => coinListStore.refresh()}
         refreshing={false}
         onEndReached={() => coinListStore.getNextPage()}
         onEndReachedThreshold={1}
-        getItemLayout={(_, index) => ({
-          length: ITEM_HEIGHT,
-          offset: ITEM_HEIGHT * index,
-          index
-        })}
         initialNumToRender={20}
         maxToRenderPerBatch={20}
         removeClippedSubviews={true}
@@ -60,16 +61,21 @@ const CoinList = observer(() => {
   );
 });
 
-const ITEM_HEIGHT = 40;
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    fontFamily: "Helvetica Neue",
+    color: "#17181b"
   },
   header: {
     fontSize: 20,
     textAlign: "center",
-    paddingVertical: 10
+    paddingVertical: 8,
+    fontWeight: "bold",
+    backgroundColor: "#F7FBFF"
+  },
+  list: {
+    backgroundColor: "#EDF7FF"
   }
 });
 
--- src/components/CoinRow.tsx
+++ src/components/CoinRow.tsx
@@ -1,23 +1,18 @@
-import { StyleSheet, View, Text } from "react-native";
 import React from "react";
-import { formatNumber } from "../common/utils";
+import { StyleSheet, Text, View } from "react-native";
+
 import { Coin } from "../common/types";
+import { formatNumber } from "../common/utils";
 
 interface Props {
   coin: Coin;
-  index: number;
-  height: number;
 }
 
 const CoinRow: React.FC<Props> = React.memo((props: Props) => {
-  const { coin, index, height } = props;
-
-  const styles = createStyles(height);
+  const { coin } = props;
+  const changeColor = coin.priceChange < 0 ? "#d94040" : "#009e73";
   return (
     <View style={styles.row}>
-      <Text style={styles.indexCell} numberOfLines={1}>
-        {index + 1}
-      </Text>
       <Text style={styles.coinSymbol} numberOfLines={1}>
         {coin.symbol}
       </Text>
@@ -25,54 +20,51 @@ const CoinRow: React.FC<Props> = React.memo((props: Props) => {
         {coin.name}
       </Text>
       <Text style={styles.price} numberOfLines={1}>
-        {formatNumber(coin.price, 4)}
+        ${formatNumber(coin.price, 4)}
       </Text>
-      <Text style={styles.priceChange} numberOfLines={1}>
+      <Text
+        style={[styles.priceChange, { color: changeColor }]}
+        numberOfLines={1}
+      >
         {formatNumber(coin.priceChange, 2)}%
       </Text>
     </View>
   );
 });
 
-const createStyles = height =>
-  StyleSheet.create({
-    row: {
-      flexDirection: "row",
-      flex: 1,
-      height: height - 6,
-      alignItems: "center",
-      alignContent: "space-between",
-      paddingVertical: 3,
-      marginVertical: 3,
-      marginHorizontal: 6,
-      backgroundColor: "grey"
-    },
-    indexCell: {
-      paddingHorizontal: 5,
-      flexBasis: 35,
-      textAlign: "right",
-      fontWeight: "bold"
-    },
-    coinSymbol: {
-      paddingHorizontal: 5,
-      flexBasis: 60,
-      fontSize: 13
-    },
-    coinName: {
-      paddingHorizontal: 5,
-      flex: 12,
-      fontSize: 15
-    },
-    price: {
-      paddingHorizontal: 5,
-      flex: 6,
-      textAlign: "right"
-    },
-    priceChange: {
-      paddingHorizontal: 5,
-      flex: 4,
-      textAlign: "right"
-    }
-  });
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    flex: 1,
+    height: 36,
+    alignItems: "center",
+    alignContent: "space-between",
+    paddingVertical: 3,
+    marginTop: 3,
+    marginHorizontal: 3,
+    backgroundColor: "#FFFFFF"
+  },
+  coinSymbol: {
+    paddingHorizontal: 5,
+    flexBasis: 60,
+    fontSize: 13
+  },
+  coinName: {
+    paddingHorizontal: 5,
+    flex: 10,
+    fontSize: 15
+  },
+  price: {
+    paddingHorizontal: 5,
+    flex: 6,
+    textAlign: "right",
+    color: "#1070e0"
+  },
+  priceChange: {
+    paddingHorizontal: 5,
+    flex: 4,
+    textAlign: "right"
+  }
+});
 
 export default CoinRow;
